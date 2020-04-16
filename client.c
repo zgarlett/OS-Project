07a772f78139918e9a25fc5b95f1286b7facd76e
zgarlett <zachary.garlett@okstate.edu>
2020-04-16 15:52:58 -0500

    No client.h file need
    This will be the file a client with execute

--- client.c
+++ client.c
@@ -1,4 +1,13 @@
-#include "client.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+//Define port to check for server connection.
+#define PORT 8080
 
 void connectToServer()
 {
