9b6d89cb50e88fcc840dfcb5d4336adae887616e
iwolfking <anethercake@gmail.com>
2020-04-25 14:43:52 -0500

    Merge client.c from my branch

--- client.c
+++ client.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
-//Define port to check for server connection.
-
-
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 void connectToServer();
-
-char buffer[1024];
-char answer[1024];
 
 int main(int argc, char const *argv[]){
 
@@ -21,46 +15,62 @@ int main(int argc, char const *argv[]){
     return 0;
 }
 
+
 void connectToServer(){
-    //Initialize some variables and the message client replies back to server with.
-    int sock = 0;
-    int portPosition = 0;
-    int readVal;
-    int ports[4] = {8181,8182,8183,8184};
-    struct sockaddr_in serv_addr;
 
+    int clientSocket, ret;
+    struct sockaddr_in serverAddr;
+    char buffer[1024];
+	char answer[1024];
+	int ports[4] = {8081,8182,8183,8184};
+	int portPosition = 0;
 
-    //Create a socket for the client.
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
+	//Create socket for the client
+    if((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("Client socket creation failure.\n");
+		exit(1);
+	}
+    printf("Client socket has been opened successfully.\n");
+
+    memset(&serverAddr, '\0', sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(ports[portPosition]);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    while(connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		serv_addr.sin_port = htons(ports[++portPosition]);
+		printf("Trying next service center\n");
+		if(portPosition > 3)
+		{
+			printf("Failed to find service center to connect to.");
+			exit(1);
+		}
+	}
+    printf("Connected to server successfully, please input whether this client is a buyer or seller.\n");
+    printf("You may type 'exit' to end the connection at any time.\n");
+
+    //Send and receive loop
+    while(1){
+        printf("Input: \t");
+        scanf("%s", &buffer[0]);
+        send(clientSocket, buffer, strlen(buffer), 0);
+
+        //When we send out exit, we go ahead and close the socket and exit, server will respond and close its connection with the client as well.
+        if(strcmp(buffer, "exit") == 0){
+            close(clientSocket);
+            printf("Disconnected from server.\n");
+            exit(1);
+        }
+        if(recv(clientSocket, buffer, 1024, 0) < 0){
+            printf("Error in receiving data.\n");
+        }
+        else
+            {
+            printf("Server: \t%s\n", buffer);
+        }
     }
 
-    //Set some properties of the server address we are connecting to.
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(ports[portPosition]);
-
-    // Convert IP address to binary.
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-    }
-
-    //Connect to our server socket.
-    while(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-      serv_addr.sin_port = htons(ports[++portPosition]);
-      printf("Trying next service center\n");
-      if (portPosition > 3){
-        printf("\nConnection Failed \n");
-        exit(0);
-      }
-    }
-
-        printf("\nConnection success.\n");
-        readVal = read(sock, buffer, 1024);
-        printf("%s\n",buffer);
-        scanf("%s", answer);
-        send(sock, answer ,strlen(answer),0);
-
-    exit(0);
+    return 0;
 }
